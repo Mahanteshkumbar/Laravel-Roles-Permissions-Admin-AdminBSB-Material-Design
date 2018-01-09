fe75eaa16510726d9ba67926f4cd567e463610bd
Mahantesh Kumbar <mahantesh.kumbar9@gmail.com>
2018-01-09 17:07:59 +0530

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>VD Jewellery</title>
+    <title>Your Title</title>
     <!-- Favicon-->
     <link rel="icon" href="favicon.ico" type="image/x-icon">
 
