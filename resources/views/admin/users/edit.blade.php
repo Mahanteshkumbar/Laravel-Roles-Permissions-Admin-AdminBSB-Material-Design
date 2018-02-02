82e3ebba463224fad3ddfbe3168bb2220e1f55ec
Mahantesh Kumbar <mahantesh.kumbar9@gmail.com>
2018-02-02 10:54:03 +0530

    Update edit.blade.php
    removed password option from user edit

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -80,18 +80,7 @@
                                     @if ($errors->has('email'))
                                         <label id="email-error" class="error" for="email">{{ $errors->first('email') }}</label>
                                     @endif
-                                </div>
-                                <div class="input-group">
-                                    <span class="input-group-addon">
-                                    <i class="material-icons">lock</i>
-                                    </span>
-                                    <div class="form-line {{ $errors->has('password') ? ' error' : '' }}">
-                                        <input type="password" class="form-control" name="password" placeholder="Password" required>
-                                    </div>
-                                    @if ($errors->has('password'))
-                                        <label id="password-error" class="error" for="name">{{ $errors->first('password') }}</label>
-                                    @endif
-                                </div>
+                                </div>                               
                                 <div class="form-group form-float">
                                     <label class="form-label">Roles</label>
                                     <select class="form-control show-tick" name="roles[]" id="you" multiple required>
